37b3d4157d34be298bc392c71b9d583b0f72dff9
BolasLien <ltzuen@gmail.com>
2020-07-08 15:55:55 +0800

    Quasar建置

---
+++ src/components/ExpansionItem.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="q-pa-md" style="max-width: 350px">
+    <q-list bordered class="rounded-borders">
+      <q-expansion-item
+        expand-separator
+        icon="store"
+        label="小閣樓"
+        caption="網站內容管理"
+        default-opened
+        :content-inset-level="0.5"
+      >
+      <q-expansion-item
+        expand-separator
+        icon="home"
+        label="首頁"
+        caption="首頁管理"
+        default-opened
+        :content-inset-level="0.5"
+      >
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-expansion-item>
+
+      </q-expansion-item>
+
+      <q-expansion-item :content-inset-level="0.5" expand-separator icon="mail" label="Inbox" caption="5 unread emails" default-opened>
+        <q-expansion-item expand-separator :content-inset-level="0.5" icon="receipt" label="Receipts">
+
+          <q-expansion-item label="Today" :content-inset-level="0.5">
+            <q-card>
+              <q-card-section>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                eveniet doloribus ullam aliquid.
+              </q-card-section>
+            </q-card>
+          </q-expansion-item>
+
+          <q-expansion-item label="Yesterday" :content-inset-level="0.5">
+            <q-card>
+              <q-card-section>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                eveniet doloribus ullam aliquid.
+              </q-card-section>
+            </q-card>
+          </q-expansion-item>
+
+        </q-expansion-item>
+
+        <q-expansion-item :content-inset-level="0.5" expand-separator icon="schedule" label="Postponed">
+          <q-card>
+            <q-card-section>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+              commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+              eveniet doloribus ullam aliquid.
+            </q-card-section>
+          </q-card>
+        </q-expansion-item>
+
+      </q-expansion-item>
+    </q-list>
+  </div>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+export default {
+  name: 'ExpansionItem',
+
+  components: {
+    EssentialLink
+  },
+
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: [
+        {
+          title: 'Carousel',
+          caption: '輪播圖',
+          icon: 'image',
+          to: 'Carousel'
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/pages/Carousel.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+  <div class="q-pa-md">
+    <q-carousel
+      arrows
+      animated
+      v-model="slide"
+      height="400px"
+    >
+      <q-carousel-slide name="first" img-src="https://cdn.quasar.dev/img/mountains.jpg">
+        <div class="absolute-bottom custom-caption">
+          <div class="text-h2">First stop</div>
+          <div class="text-subtitle1">Mountains</div>
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="second" img-src="https://cdn.quasar.dev/img/parallax1.jpg">
+        <div class="absolute-bottom custom-caption">
+          <div class="text-h2">Second stop</div>
+          <div class="text-subtitle1">Famous City</div>
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="third" img-src="https://cdn.quasar.dev/img/parallax2.jpg">
+        <div class="absolute-bottom custom-caption">
+          <div class="text-h2">Third stop</div>
+          <div class="text-subtitle1">Famous Bridge</div>
+        </div>
+      </q-carousel-slide>
+    </q-carousel>
+  </div>
+    <div class="q-pa-md">
+      <div class="q-gutter-md row items-start">
+        <q-uploader
+        style="max-width: 300px"
+        url="http://localhost:4444/upload"
+        label="Restricted to images"
+        multiple
+        accept=".jpg, image/*"
+        @rejected="onRejected"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      slide: 'first'
+    }
+  },
+  methods: {
+    onRejected (rejectedEntries) {
+      // Notify plugin needs to be installed
+      // https://quasar.dev/quasar-plugins/notify#Installation
+      this.$q.notify({
+        type: 'negative',
+        message: `${rejectedEntries.length} file(s) did not pass validation constraints`
+      })
+    }
+  }
+}
+</script>
