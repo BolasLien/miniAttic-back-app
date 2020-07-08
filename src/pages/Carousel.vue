4424cf7e3fad69d3ee7fb3c155fa39acc7fda681
BolasLien <ltzuen@gmail.com>
2020-07-08 16:34:04 +0800

    測試項目移除

--- src/pages/Carousel.vue
+++ src/pages/Carousel.vue
@@ -1,32 +1,6 @@
 <template>
   <div>
-  <div class="q-pa-md">
-    <q-carousel
-      arrows
-      animated
-      v-model="slide"
-      height="400px"
-    >
-      <q-carousel-slide name="first" img-src="https://cdn.quasar.dev/img/mountains.jpg">
-        <div class="absolute-bottom custom-caption">
-          <div class="text-h2">First stop</div>
-          <div class="text-subtitle1">Mountains</div>
-        </div>
-      </q-carousel-slide>
-      <q-carousel-slide name="second" img-src="https://cdn.quasar.dev/img/parallax1.jpg">
-        <div class="absolute-bottom custom-caption">
-          <div class="text-h2">Second stop</div>
-          <div class="text-subtitle1">Famous City</div>
-        </div>
-      </q-carousel-slide>
-      <q-carousel-slide name="third" img-src="https://cdn.quasar.dev/img/parallax2.jpg">
-        <div class="absolute-bottom custom-caption">
-          <div class="text-h2">Third stop</div>
-          <div class="text-subtitle1">Famous Bridge</div>
-        </div>
-      </q-carousel-slide>
-    </q-carousel>
-  </div>
+
     <div class="q-pa-md">
       <div class="q-gutter-md row items-start">
         <q-uploader
