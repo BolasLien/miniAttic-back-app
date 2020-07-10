b582a4374e606e7d6b1c59b9b4133d29080cbfac
BolasLien <ltzuen@gmail.com>
2020-07-10 09:27:05 +0800

    小閣樓新鮮事-編輯區塊

--- src/pages/Intro.vue
+++ src/pages/Intro.vue
@@ -1,30 +1,49 @@
 <template>
   <div>
-    <div>
-      <img src="http://220.128.133.15/s1090109/miniattic/assets/img/index-intro-1.jpg" width="350">
-      <p>Intro-1</p>
-      <q-input outlined v-model="data[0].textModel" placeholder="圖片描述" :hint="data[0].text" />
-      <q-input outlined v-model="data[0].hrefModel" placeholder="超連結" :hint="data[0].href" />
-      <q-btn push color="primary" label="保存" />
-      <q-toggle
-        v-model="toggles[0]"
-        checked-icon="check"
-        color="green"
-        unchecked-icon="clear"
-        label="是否顯示"
-      />
-      <!-- <div class="q-pa-md">
-        <div class="q-gutter-md row items-start">
-          <q-uploader
-          style="max-width: 300px"
-          url="http://localhost:4444/upload"
-          label="Restricted to images"
-          multiple
-          accept=".jpg, image/*"
-          @rejected="onRejected"
-          />
-        </div>
-      </div> -->
+    <div class="row">
+      <div class="col-4">
+        <img src="http://220.128.133.15/s1090109/miniattic/assets/img/index-intro-1.jpg" width="350">
+        <p>Intro-1</p>
+        <q-input outlined v-model="data[0].textModel" placeholder="圖片描述" :hint="data[0].text" />
+        <q-input outlined v-model="data[0].hrefModel" placeholder="超連結" :hint="data[0].href" />
+        <q-btn push color="primary" label="保存" />
+        <q-toggle
+          v-model="toggles[0]"
+          checked-icon="check"
+          color="green"
+          unchecked-icon="clear"
+          label="是否顯示"
+        />
+      </div>
+      <div class="col-4">
+        <img src="http://220.128.133.15/s1090109/miniattic/assets/img/index-intro-2.jpg" width="350">
+        <p>Intro-1</p>
+        <q-input outlined v-model="data[0].textModel" placeholder="圖片描述" :hint="data[0].text" />
+        <q-input outlined v-model="data[0].hrefModel" placeholder="超連結" :hint="data[0].href" />
+        <q-btn push color="primary" label="保存" />
+        <q-toggle
+          v-model="toggles[0]"
+          checked-icon="check"
+          color="green"
+          unchecked-icon="clear"
+          label="是否顯示"
+        />
+      </div>
+      <div class="col-4">
+        <img src="http://220.128.133.15/s1090109/miniattic/assets/img/index-intro-2.jpg" width="350">
+        <p>Intro-1</p>
+        <q-input outlined v-model="data[0].textModel" placeholder="圖片描述" :hint="data[0].text" />
+        <q-input outlined v-model="data[0].hrefModel" placeholder="超連結" :hint="data[0].href" />
+        <q-btn push color="primary" label="保存" />
+        <q-toggle
+          v-model="toggles[0]"
+          checked-icon="check"
+          color="green"
+          unchecked-icon="clear"
+          label="是否顯示"
+        />
+      </div>
+
     </div>
   </div>
 </template>
