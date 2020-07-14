35d97983998c0dc7a8b89902f7a9ec36fa61af99
Bolas <ltzuen@gmail.com>
2020-07-14 21:45:45 +0800

    把標題設定抽象出來

---
+++ src/components/Heading.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="heading">
+    <p>{{title.item}}</p>
+      <q-input outlined v-model="title.titleModel" placeholder="標題" :hint="title.title" />
+      <q-input outlined v-model="title.subtitleModel" placeholder="副標題" :hint="title.subtitle" />
+    <q-btn push color="primary" label="保存" @click="submit(title)"/>
+  </div>
+</template>
+
+<script>
+export default {
+  inject: ['reload', 'submit', 'upload'],
+  name: 'Heading',
+  props: {
+    title: Object
+  }
+}
+</script>
