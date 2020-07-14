deff25bbecc2a8d339ea36b644d50e52d9a7934d
BolasLien <ltzuen@gmail.com>
2020-07-14 15:35:35 +0800

    router

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,8 +6,13 @@ const routes = [
     children: [
       { path: '', component: () => import('pages/Index.vue') },
       { path: 'home', component: () => import('pages/Home.vue') },
-      { path: 'carousel', component: () => import('pages/Carousel.vue') },
-      { path: 'intro', component: () => import('pages/Intro.vue') },
+      { path: 'carousel', component: () => import('pages/home/Carousel.vue') },
+      { path: 'intro', component: () => import('pages/home/Intro.vue') },
+      { path: 'miniProducts', component: () => import('pages/home/miniProducts.vue') },
+      { path: 'miniAbout', component: () => import('pages/home/miniAbout.vue') },
+      { path: 'CallToAction', component: () => import('pages/home/CallToAction.vue') },
+      { path: 'miniRecommend', component: () => import('pages/home/miniRecommend.vue') },
+      { path: 'miniFooter', component: () => import('pages/home/miniFooter.vue') },
       { path: 'login', component: () => import('pages/Login.vue') },
       { path: 'Reg', component: () => import('pages/Reg.vue') }
     ]
