df167382cc696c7fa9d27a39ea5195cb111d7478
BolasLien <ltzuen@gmail.com>
2020-07-17 16:39:23 +0800

    讓input變成 inline-block

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="heading">
     <p>{{title.item}}</p>
-      <q-input outlined v-model="title.titleModel" placeholder="標題" :hint="title.title" />
-      <q-input outlined v-model="title.subtitleModel" placeholder="副標題" :hint="title.subtitle" />
+      <q-input class="inline on-left" outlined v-model="title.titleModel" placeholder="標題" :hint="title.title" />
+      <q-input class="inline on-left" outlined v-model="title.subtitleModel" placeholder="副標題" :hint="title.subtitle" />
     <q-btn push color="primary" label="保存" @click="submit(title)"/>
   </div>
 </template>
