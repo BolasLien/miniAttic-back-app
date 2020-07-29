6de9074d5383b8fa7cc21d98bf5506c692d0f8e8
BolasLien <ltzuen@gmail.com>
2020-07-29 16:33:00 +0800

    沒登入自動導到login畫面

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import routes from './routes'
+import store from '../store/index'
 
 Vue.use(VueRouter)
 
@@ -24,6 +25,14 @@ export default function (/* { store, ssrContext } */) {
     // quasar.conf.js -> build -> publicPath
     mode: process.env.VUE_ROUTER_MODE,
     base: process.env.VUE_ROUTER_BASE
+  })
+
+  Router.beforeEach((to, from, next) => {
+    if (to.meta.login && !store().state.user) {
+      next('/login')
+    } else {
+      next()
+    }
   })
 
   return Router
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,17 +4,80 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') },
-      { path: 'home', component: () => import('pages/Home.vue') },
-      { path: 'carousel', component: () => import('pages/home/Carousel.vue') },
-      { path: 'intro', component: () => import('pages/home/Intro.vue') },
-      { path: 'miniProducts', component: () => import('pages/home/miniProducts.vue') },
-      { path: 'miniAbout', component: () => import('pages/home/miniAbout.vue') },
-      { path: 'CallToAction', component: () => import('pages/home/CallToAction.vue') },
-      { path: 'miniRecommend', component: () => import('pages/home/miniRecommend.vue') },
-      { path: 'miniFooter', component: () => import('pages/home/miniFooter.vue') },
-      { path: 'Products', component: () => import('pages/shop/Products.vue') },
-      { path: 'Category', component: () => import('pages/shop/Category.vue') },
+      {
+        path: '',
+        component: () => import('pages/Index.vue'),
+        meta: {
+          login: true
+        }
+      },
+      {
+        path: 'home',
+        component: () => import('pages/Home.vue')
+      },
+      {
+        path: 'carousel',
+        component: () => import('pages/home/Carousel.vue'),
+        meta: {
+          login: true
+        }
+      },
+      {
+        path: 'intro',
+        component: () => import('pages/home/Intro.vue'),
+        meta: {
+          login: true
+        }
+      },
+      {
+        path: 'miniProducts',
+        component: () => import('pages/home/miniProducts.vue'),
+        meta: {
+          login: true
+        }
+      },
+      {
+        path: 'miniAbout',
+        component: () => import('pages/home/miniAbout.vue'),
+        meta: {
+          login: true
+        }
+      },
+      {
+        path: 'CallToAction',
+        component: () => import('pages/home/CallToAction.vue'),
+        meta: {
+          login: true
+        }
+      },
+      {
+        path: 'miniRecommend',
+        component: () => import('pages/home/miniRecommend.vue'),
+        meta: {
+          login: true
+        }
+      },
+      {
+        path: 'miniFooter',
+        component: () => import('pages/home/miniFooter.vue'),
+        meta: {
+          login: true
+        }
+      },
+      {
+        path: 'Products',
+        component: () => import('pages/shop/Products.vue'),
+        meta: {
+          login: true
+        }
+      },
+      {
+        path: 'Category',
+        component: () => import('pages/shop/Category.vue'),
+        meta: {
+          login: true
+        }
+      },
       { path: 'login', component: () => import('pages/Login.vue') },
       { path: 'Reg', component: () => import('pages/Reg.vue') }
     ]
